2e0d84deba9ba7f8085f5b2b1dc385939203f37c
Sneha Elizabeth Cherian <112601285+sneha-dataai@users.noreply.github.com>
2022-09-01 15:27:37 -0400

    Update streamlit_app.py
    removed line of code

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ import requests
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
 
 # take the json and normalize - expand the attributes
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
