e3256d7644255617c50ab2446d4c61792c97479a
Sneha Elizabeth Cherian <112601285+sneha-dataai@users.noreply.github.com>
2022-09-01 17:51:51 -0400

    Update streamlit_app.py
    Connection from snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,13 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # load the table into a dataframe
 streamlit.dataframe(fruityvice_normalized)
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 #st.markdown('<p class="big-font">Breakfast Menu</p>', unsafe_allow_html=True)
 #st.markdown('<p class="small-font">Omega 3 & blueberry Oatmeal</p>', unsafe_allow_html=True)
 #st.markdown('<p class="small-font">Kale, Spinach & Rocket Smoothie</p>', unsafe_allow_html=True)
