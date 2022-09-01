dbf21b234724ced123f247e6e8ad0db26366be70
Sneha Elizabeth Cherian <112601285+sneha-dataai@users.noreply.github.com>
2022-09-01 15:24:00 -0400

    Update streamlit_app.py
    added new section header

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,11 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 import requests
+
+streamlit.header("Fruityvice Fruit Advice!")
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
 #st.markdown('<p class="big-font">Breakfast Menu</p>', unsafe_allow_html=True)
