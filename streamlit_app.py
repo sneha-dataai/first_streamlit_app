22de4af12ae38f3b86f230e1b54ebda58e4a4271
Sneha Elizabeth Cherian <112601285+sneha-dataai@users.noreply.github.com>
2022-09-01 00:06:36 -0400

    Update streamlit_app.py
    Tried without styles

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,13 @@ st.markdown("""
 }
 </style>
 """, unsafe_allow_html=True)
-st.markdown('<p class="big-font">Breakfast Menu</p>', unsafe_allow_html=True)
-st.markdown('<p class="small-font">Omega 3 & blueberry Oatmeal</p>', unsafe_allow_html=True)
-st.markdown('<p class="small-font">Kale, Spinach & Rocket Smoothie</p>', unsafe_allow_html=True)
-st.markdown('<p class="small-font">Hard-Boiled Free-Range Egg</p>', unsafe_allow_html=True)
+st.header('Breakfast Menu')
+st.text('Omega 3 & blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
+
+
+#st.markdown('<p class="big-font">Breakfast Menu</p>', unsafe_allow_html=True)
+#st.markdown('<p class="small-font">Omega 3 & blueberry Oatmeal</p>', unsafe_allow_html=True)
+#st.markdown('<p class="small-font">Kale, Spinach & Rocket Smoothie</p>', unsafe_allow_html=True)
+#st.markdown('<p class="small-font">Hard-Boiled Free-Range Egg</p>', unsafe_allow_html=True)
