933c0ff43331d073518ec8f98c5d03ef695c9cef
Sneha Elizabeth Cherian <112601285+sneha-dataai@users.noreply.github.com>
2022-09-01 17:53:32 -0400

    Update streamlit_app.py
    adding sql queries

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,9 +52,9 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
 
 #st.markdown('<p class="big-font">Breakfast Menu</p>', unsafe_allow_html=True)
