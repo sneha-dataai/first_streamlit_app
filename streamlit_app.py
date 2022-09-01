46fdabe34fd15d91c5012aea0d23f54d3fbb25b6
Sneha Elizabeth Cherian <112601285+sneha-dataai@users.noreply.github.com>
2022-09-01 18:44:47 -0400

    Update streamlit_app.py
    Heading style

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 #streamlit.stop()
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 #snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
