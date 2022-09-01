48b0bc9b1721b0b7fc7f509fd87f67cf6445fa50
Sneha Elizabeth Cherian <112601285+sneha-dataai@users.noreply.github.com>
2022-09-01 00:00:53 -0400

    Update streamlit_app.py
    Added style

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.markdown("""
 <style>
 .big-font {
-    font-size:30px !important;
+    font-size:50px !important;
 }
 </style>
-""", unsafe_allow_html=True')
+""", unsafe_allow_html=True)
+st.markdown('<p class="big-font">Breakfast Menu</p>', unsafe_allow_html=True)
